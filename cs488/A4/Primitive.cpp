d0cffcfe4faf8cae13241592188e76d25c8676ac
xilingwu <wuxiling@outlook.com>
2019-05-13 00:50:27 -0400

    initial provided code

---
+++ cs488/A4/Primitive.cpp
@@ -0,0 +1,23 @@
+// Winter 2019
+
+#include "Primitive.hpp"
+
+Primitive::~Primitive()
+{
+}
+
+Sphere::~Sphere()
+{
+}
+
+Cube::~Cube()
+{
+}
+
+NonhierSphere::~NonhierSphere()
+{
+}
+
+NonhierBox::~NonhierBox()
+{
+}
